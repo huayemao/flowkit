482c218315cae5a7d67a983f56e09316f8a4ed95
huayemao <huayemao4g@gmail.com>
2025-07-03 21:41:54 +0800

    refactor: tool-renderer.tsx 采用组件映射表（componentMap）方式

--- src/components/tool-renderer.tsx
+++ src/components/tool-renderer.tsx
@@ -9,27 +9,21 @@ interface ToolRendererProps {
   tool: Tool;
 }
 
+const componentMap: Record<string, React.ComponentType<any>> = {
+  TextRemoveNewlines,
+  TextOcr,
+  SvgScaler,
+  AutoTrimImage,
+  // 后续新组件只需在此注册
+};
+
 export function ToolRenderer({ tool }: ToolRendererProps) {
-  switch (tool.type) {
-    case "component":
-      switch (tool.component) {
-        case "TextRemoveNewlines":
-          return <TextRemoveNewlines />;
-        case "TextOcr":
-          return <TextOcr />;
-        case "SvgScaler":
-          return <SvgScaler />;
-        case "AutoTrimImage":
-          return <AutoTrimImage />;
-        default:
-          return <div>未知的组件类型: {tool.component}</div>;
-      }
-    case "web-app":
-      if (tool.url) {
-        return <WebAppEmbed title={tool.description} url={tool.url} />;
-      }
-      return <div>无效的 Web 应用 URL</div>;
-    default:
-      return <div>未知的工具类型: {tool.type}</div>;
+  if (tool.type === 'component') {
+    const Comp = componentMap[tool.component!];
+    return Comp ? <Comp /> : <div>未知的组件类型: {tool.component}</div>;
   }
+  if (tool.type === 'web-app' && tool.url) {
+    return <WebAppEmbed title={tool.description} url={tool.url} />;
+  }
+  return <div>未知的工具类型: {tool.type}</div>;
 } 
