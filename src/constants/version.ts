6dc813eadf2218d3a731ceabbbdc44ca6fb460ba
huayemao <huayemao4g@gmail.com>
2025-09-07 11:39:47 +0800

    release: 发布 flowkit 0.3.0

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,6 +1,6 @@
 // 版本常量
 // 这个文件会在构建时被 update-version.cjs 脚本更新
-export const APP_VERSION = "0.2.9";
+export const APP_VERSION = "0.3.0";
 
 // 格式化版本号，添加 "v" 前缀
 export const APP_VERSION_DISPLAY = `v${APP_VERSION}`;
