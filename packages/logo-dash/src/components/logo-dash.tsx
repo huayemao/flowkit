13621bd7654c1f6dac2894653c204b33078229c4
huayemao <huayemao4g@gmail.com>
2025-09-26 21:34:20 +0800

    fix(logo-dash): 修复随机生成时重排的问题

--- packages/logo-dash/src/components/logo-dash.tsx
+++ packages/logo-dash/src/components/logo-dash.tsx
@@ -271,7 +271,7 @@ export function LogoDash({ className, onLogoCreated }: LogoMakerProps) {
         </div>
 
         {/* 右侧：设置区域 */}
-        <div className="space-y-6">
+        <div className="space-y-6 lg:h-[88vh]">
           <Card>
             <CardHeader>
               <CardTitle>{t('logoDash.icon')}</CardTitle>
