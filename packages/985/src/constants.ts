e391caa5599fbec11008d2e26ef2c41deaf086e1
huayemao <huayemao4g@gmail.com>
2026-08-28 14:40:00 +0800

    chore: 移除无用的项目

--- packages/985/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const DOMAIN_NAME = '985.utities.online';
-export const SITE_ID = '985';
--- packages/985/src/index.ts
+++
@@ -1,4 +0,0 @@
-import './index.css'
-import UniversityMap from './components/UniversityMap'
-export { UniversityMap }
-export default UniversityMap
